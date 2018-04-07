78dcc9f0d0a49dfb352854639d00246fe4eb14d1
SL <SL@DESKTOP-LIQIUD5>
2018-04-07 14:16:24 +0800

    dao整合

---
+++ xstore-manage/xstore-manage-mapper/src/main/java/com/sl/mapper/TbOrderMapper.java
@@ -0,0 +1,31 @@
+package com.sl.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import com.sl.pojo.TbOrder;
+import com.sl.pojo.TbOrderExample;
+
+public interface TbOrderMapper {
+    int countByExample(TbOrderExample example);
+
+    int deleteByExample(TbOrderExample example);
+
+    int deleteByPrimaryKey(String orderId);
+
+    int insert(TbOrder record);
+
+    int insertSelective(TbOrder record);
+
+    List<TbOrder> selectByExample(TbOrderExample example);
+
+    TbOrder selectByPrimaryKey(String orderId);
+
+    int updateByExampleSelective(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByExample(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByPrimaryKeySelective(TbOrder record);
+
+    int updateByPrimaryKey(TbOrder record);
+}
